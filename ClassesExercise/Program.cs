421c1391e95709d6ca8060e6b19d064b78852632
TheWookersquatch <ksprettygurl@gmail.com>
2021-06-18 10:44:53 -0600

    initial commit

---
+++ ClassesExercise/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ClassesExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var car1 = new Car();
+            car1.Make = " Honda";
+            car1.Model = "Accord";
+            car1.Year = "2003";
+            Console.WriteLine($"My {car1.Year} {car1.Make} {car1.Model} is on its last legs.");
+        }
+    }
+}
